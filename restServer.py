1107b8ab7eab39f7e8cbf8dd622c95c0df7c67e0
leethemann <Dietcokke@Lees-MacBook-Pro.local>
2017-07-11 14:37:52 -0700

    adding create user

--- restServer.py
+++ restServer.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify, abort, make_response, request
 
 app = Flask(__name__)
 
@@ -29,6 +29,24 @@ def get_user(user_id):
     if len(user) == 0:
         abort(404)
     return jsonify({'user': user[0]})
+
+@app.errorhandler(404)
+def not_found(error):
+        return make_response(jsonify({'error': 'Not found'}), 404)
+
+@app.route('/projectdread/api/v1.0/users', methods=['POST'])
+def create_take():
+    if not request.json or not 'username' in request.json:
+        abort(400)
+    user = {
+        'id': users[-1]['id'] + 1,
+        'username': request.json['username'],
+        'race': request.json['race'],
+        'gold': request.json['gold'],
+        'online': False
+    }
+    users.append(user)
+    return jsonify({'user': user}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
